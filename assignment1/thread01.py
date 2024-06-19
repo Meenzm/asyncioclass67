3a5452eb19f84f1b8632a3c53ea161996aac291a
Meenzm <6510301024@cdti.ac.th>
2024-06-19 11:28:08 +0700

    Update thread01.py

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -1,1 +1,18 @@
 # running a function in another thread
+from time import sleep, ctime
+from threading import Thread
+
+# a custom function that block for a moe=ment
+def task():
+    #block for a moment
+    sleep(1)
+    # display a message
+    print(f'{ctime()} This is from another thread')
+    
+# crate a thread
+thread = Thread(target=task)
+#run the thrad
+thread.start()
+#wait for the thread to finish
+print(f'{ctime()} Waiting for the thread...')
+thread.join()
