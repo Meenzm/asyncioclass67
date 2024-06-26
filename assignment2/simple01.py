028e080e23e9a73acf28a969158148f64bb36597
Meenzm <6510301024@cdti.ac.th>
2024-06-26 12:04:18 +0700

    ส่งงาน

--- assignment2/simple01.py
+++ assignment2/simple01.py
@@ -1,2 +1,19 @@
 # Synchronous cooking
 # 1 kitchen 1 chefs 1 dish
+from time import sleep, ctime, time
+
+# cooking synchronose
+def cooking(index):
+    print(f"{ctime()} Kitchen-{index}   : Begin cook...")
+    sleep(2)
+    print(f'{ctime()} Kitchen-{index}   : Cooking done!')
+
+if __name__ == '__main__':
+    # Begin of main thread
+    print(f'{ctime()} Main   : Start cooking.')
+    start_time = time()
+    # Cooking
+    cooking(0)
+    
+    duration = time() - start_time
+    print(f'{ctime()} Main   : Finished Cooking duration in {duration:02f} seconds.')   
--- assignment2/simple02.py
+++ assignment2/simple02.py
@@ -1,2 +1,20 @@
 # Synchronous cooking
 # 2 kitchen 2 chefs 2 dishes
+from time import sleep, ctime, time
+
+# cooking synchronose
+def cooking(index):
+    print(f"{ctime()} Kitchen-{index}   : Begin cook...")
+    sleep(2)
+    print(f'{ctime()} Kitchen-{index}   : Cooking done!')
+
+if __name__ == '__main__':
+    # Begin of main thread
+    print(f'{ctime()} Main   : Start cooking.')
+    start_time = time()
+    # Cooking for each dish
+    for index in range(2):
+        cooking(index)
+    
+    duration = time() - start_time
+    print(f'{ctime()} Main   : Finished Cooking duration in {duration:02f} seconds.')   
