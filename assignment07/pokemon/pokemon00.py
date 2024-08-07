a3256dbe8b13f609bd9fdec025dd57ba91381883
Meenzm <6510301024@cdti.ac.th>
2024-08-07 09:33:45 +0700

    Update pokemon00.py

--- assignment07/pokemon/pokemon00.py
+++ assignment07/pokemon/pokemon00.py
@@ -7,8 +7,9 @@ pokemonmove_directort = 'assignment07\pokemon\pokemonmove'
 
 async def main():
     # Read the contents of the json file.
-    async with aiofiles.open(f'{pokemonapi_directory}', mode='r') as f: contents = await f.read()
+    async with aiofiles.open(f'{pokemonapi_directory}/articuno.json', mode='r') as f: contents = await f.read()
     print(contents)
+    
     
 asyncio.run(main())
     
