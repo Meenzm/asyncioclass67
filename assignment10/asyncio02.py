c1492aeaf5e86cd5d790a253e4977ec5ff50db45
Meenzm <6510301024@cdti.ac.th>
2024-08-28 10:31:24 +0700

    ส่งงาน 28/8/67

--- assignment10/asyncio02.py
+++ assignment10/asyncio02.py
@@ -1,10 +1,13 @@
 # example of using an asyncio queue without blocking
 from random import random
 import asyncio
+import time
  
 # coroutine to generate work
 async def producer(queue):
     print('Producer: Running')
+    start_time_pc = time.perf_counter()
+    # start time
     # generate work
     for i in range(10):
         # generate a value
@@ -18,11 +21,17 @@ async def producer(queue):
         await queue.put(value)
     # send an all done signal
     await queue.put(None)
+    end_time_pc = time.perf_counter() - start_time_pc
     print('Producer: Done')
+    print(f"Produce  time: {end_time_pc} seconds")
+
+    
+
  
 # coroutine to consume work
 async def consumer(queue):
     print('Consumer: Running')
+    start_time_cm = time.perf_counter()
     # consume work
     while True:
         # get a unit of work without blocking
@@ -37,15 +46,21 @@ async def consumer(queue):
             break
         # report
         print(f'\t> Consumer got {item}')
+    end_time_cm = time.perf_counter() - start_time_cm
     # all done
     print('Consumer: Done')
+    print(f"Consumer  time: {end_time_cm} sec")
+
  
 # entry point coroutine
 async def main():
+    start_time = time.perf_counter()
     # create the shared queue
     queue = asyncio.Queue()
     # run the producer and consumers
     await asyncio.gather(producer(queue), consumer(queue))
+    end_time = time.perf_counter() - start_time 
+    print(f'Overall time: {end_time} sec')
  
 # start the asyncio program
 asyncio.run(main())
